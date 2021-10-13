6c5ebafdfa703c5856aea6b81816f6de912f7cf5
MOgorodnik <maksim.o@vevidi.com>
2021-10-13 18:44:22 +0300

    Added commented links

--- src/main.js
+++ src/main.js
@@ -16,3 +16,4 @@ new Vue({
 
 // https://dev.to/thecoderishi/quick-start-with-tailwind-css-334n
 // https://tailwindcss.com/docs/installation#post-css-7-compatibility-build
+// https://coderoad.ru/48515023/%D0%9E%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D1%80%D0%B0%D0%B7%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D1%85-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%BE%D0%B2-Vuejs-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D1%85-%D0%B1%D1%80%D0%B0%D1%83%D0%B7%D0%B5%D1%80%D0%BE%D0%B2
